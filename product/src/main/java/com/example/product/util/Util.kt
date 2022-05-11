56f5d8da21eabb5aeefd8039b6fb4ac68a25fb1c
jemuveyan <jemuveyan@axxess.com>
2022-05-11 23:34:10 +0100

    added id's

--- product/src/main/java/com/example/product/util/Util.kt
+++ product/src/main/java/com/example/product/util/Util.kt
@@ -2,7 +2,7 @@ package com.example.product.util
 
 import android.widget.LinearLayout
 
-const val PRODUCT_IDENTIFIER_GERMANY30 = ""
-const val PRODUCT_IDENTIFIER_US500 = ""
-const val PRODUCT_IDENTIFIER_EURO_US = ""
+const val PRODUCT_IDENTIFIER_GERMANY30 = "sb26493"
+const val PRODUCT_IDENTIFIER_US500 = "sb26496"
+const val PRODUCT_IDENTIFIER_EURO_US = "sb26502"
 const val NOT_AVAILABLE = "Not available"
