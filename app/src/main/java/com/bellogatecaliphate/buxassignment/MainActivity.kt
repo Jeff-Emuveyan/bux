81b0c700ee072c88d425920389acb1d68bbaf615
LAPTOP-PHAJIQ3D\Jeff <jeffemuveyan@gmail.com>
2022-09-17 00:52:05 +0100

    minor

--- app/src/main/java/com/bellogatecaliphate/buxassignment/MainActivity.kt
+++ app/src/main/java/com/bellogatecaliphate/buxassignment/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun disableNightMode() {
+
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
